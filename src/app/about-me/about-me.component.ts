38d1d99ec15499edcaae43245422288c23c3e480
vareversat <valentin.reversat@dev-o-sud.fr>
2020-05-05 15:14:11 +0200

    add languages

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { JsonService } from '../json.service';
+import { Component, OnInit } from "@angular/core";
+import { JsonService } from "../json.service";
 import {
   TransitionController,
   Transition,
-  TransitionDirection
-} from 'ng2-semantic-ui';
+  TransitionDirection,
+} from "ng2-semantic-ui";
 
 @Component({
-  selector: 'app-about-me',
-  templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.less']
+  selector: "app-about-me",
+  templateUrl: "./about-me.component.html",
+  styleUrls: ["./about-me.component.less"],
 })
 export class AboutMeComponent implements OnInit {
   public imageTransition = new TransitionController();
@@ -18,23 +18,31 @@ export class AboutMeComponent implements OnInit {
 
   constructor(private jsonService: JsonService) {
     this.jsonService
-      .getJSON('../../assets/data/profile.json')
-      .subscribe(data => {
+      .getJSON("../../assets/data/profile.json")
+      .subscribe((data) => {
         this.profile = data;
       });
   }
 
+  public getLanguages() {
+    var languages = " | ";
+    for (const language of this.profile.languages) {
+      languages += language.lang + " (" + language.level + ")"  + " | "
+    }
+    return languages;
+  }
+
   public animate() {
     this.imageTransition.animate(
-      new Transition('fade down', 900, TransitionDirection.In)
+      new Transition("fade down", 900, TransitionDirection.In)
     );
     this.buttonsTransition.animate(
-      new Transition('fade left', 900, TransitionDirection.In)
+      new Transition("fade left", 900, TransitionDirection.In)
     );
   }
 
   public goToURL(url: string) {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   }
 
   ngOnInit() {
