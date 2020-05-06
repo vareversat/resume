31112fabe90175c8224ef64f58bec0bed21b7839
vareversat <valentin.reversat@dev-o-sud.fr>
2020-05-06 23:24:20 +0200

    remove jquery

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,38 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import * as _ from 'lodash';
-
-declare var $: any;
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import * as _ from "lodash";
 
 @Component({
-  selector: 'app-menu-bar',
-  templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.less']
+  selector: "app-menu-bar",
+  templateUrl: "./menu-bar.component.html",
+  styleUrls: ["./menu-bar.component.less"],
 })
 export class MenuBarComponent implements OnInit {
+  public show = false;
   constructor(private router: Router) {
     this.router = router;
   }
 
   get selectedPage() {
     if (!_.isUndefined(this.router.url)) {
-      return this.router.url.replace('/', '');
+      return this.router.url.replace("/", "");
     }
   }
 
   showMenu() {
-    $('.ui.modal')
-      .modal('show')
-      ;
+    this.show = true;
   }
 
   hideMenu() {
-    $('.ui.modal')
-      .modal('hide')
-      ;
+    this.show = false;
   }
-
 
   ngOnInit() {}
 }
