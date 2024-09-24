bf80bcfc838626e654e0cf780eacc93b1280e505
Valentin REVERSAT <dev@vareversat.fr>
2024-09-24 23:17:23 +0200

    chore(deps): run ´pnpm update´

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true,
-    app: {
-        pageTransition: {name: 'page', mode: 'out-in'}
-    },
-    devtools: {enabled: true},
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
-    colorMode: {
-        preference: 'system', // default value of $colorMode.preference
-        fallback: 'dark', // fallback value if not system preference found
-        hid: 'nuxt-color-mode-script',
-        globalName: '__NUXT_COLOR_MODE__',
-        componentName: 'ColorScheme',
-        classPrefix: '',
-        classSuffix: '',
-        storageKey: 'nuxt-color-mode'
-    },
-    modules: ["@nuxt/image", "@nuxt/content", "@nuxt/ui", "@nuxtjs/color-mode", "nuxt-icon"]
+  ssr: true,
+
+  app: {
+      pageTransition: {name: 'page', mode: 'out-in'}
+  },
+
+  devtools: {enabled: true},
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+      },
+  },
+
+  colorMode: {
+      preference: 'system', // default value of $colorMode.preference
+      fallback: 'dark', // fallback value if not system preference found
+      hid: 'nuxt-color-mode-script',
+      globalName: '__NUXT_COLOR_MODE__',
+      componentName: 'ColorScheme',
+      classPrefix: '',
+      classSuffix: '',
+      storageKey: 'nuxt-color-mode'
+  },
+
+  modules: ["@nuxt/image", "@nuxt/content", "@nuxt/ui", "@nuxtjs/color-mode", "nuxt-icon"],
+  compatibilityDate: '2024-09-24'
 })
